0fcb7814b3de78da9398a8bc0d44bb04af185a6a
AlexandrKuznetsov1 <166503929+AlexandrKuznetsov1@users.noreply.github.com>
2024-04-25 17:47:22 +0700

    Add files via upload

---
+++ Modul2/homework4m2.py
@@ -0,0 +1,6 @@
+def print_params(param):
+    print((str(param)+" " + str(param)))
+ # насколько я понял задание, результат должен быть таким
+print_params('Alex')
+print_params(2024)
+print_params(20.24)
