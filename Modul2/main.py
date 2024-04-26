b8be6eb3e8859857052b51e9004aed6d6ce3bc2a
AlexandrKuznetsov1 <166503929+alexandrkuznetsov1@users.noreply.github.com>
2024-04-26 09:56:20 +0700

    исправлен стиль кода pep8

--- Modul2/main.py
+++ Modul2/main.py
@@ -1,1 +1,8 @@
-
+list_ = [9.8, 17, 'Cat', [98, 'dog'], {78, 'bear'}, {'key':12345}]
+print(list_)
+taple_ = (9.8, 17, 'Cat', [98, 'dog'], {78, 'bear'}, {'key':12345})
+print(taple_)
+set_ = {'ghb', 12, 12.7}
+print(set_)
+print(list(set_))
+print(len(list(set_)))
