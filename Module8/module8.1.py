3f4fba376f5ebbe0996a485203f7afe91f20696d
AlexandrKuznetsov1 <1@example.com>
2024-06-18 14:33:33 +0700

    Добавлено  по ValueError

--- Module8/module8.1.py
+++ Module8/module8.1.py
@@ -18,7 +18,29 @@ def add_everything_up(a, b):
     else:
         return f'{"{0:.3f}".format(itog)}'
 
-
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
 print(add_everything_up(123.456, 7))
+
+
+print('Пожалуйста, введите поочереди три целых числа. Я скажу Вам сколько одинаковых:')
+while 1:
+    try:
+        first = int(input('Введите значение first:  '))
+        second = int(input('Введите значение second:  '))
+        third = int(input('Введите значение third:  '))
+        break
+    except ValueError as exc:
+        print(f"Недопустимое значение {exc}, \nПожалуйста повторите ввод.")
+        continue
+
+if isinstance(first, int) and isinstance(second, int) and isinstance(third, int):
+    if first == second == third:
+        a = 3
+        print(f'Вы ввели {a} равных числа')
+    if first == second != third or first == third != second or second == third != first:
+        b = 2
+        print(f'Вы ввели {b} равных числа')
+    else:
+        if first != second or first != third or second != third:
+            print("Равных чисел среди введенных нет,- результат '0'")
