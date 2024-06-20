5b2dc8e99d560059f4d2254b7e921fdda67176a1
AlexandrKuznetsov1 <166503929+AlexandrKuznetsov1@users.noreply.github.com>
2024-06-20 14:14:56 +0700

    Add files via upload

---
+++ Modul6/module_6_2.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+# Домашнее задание по теме "Доступ к свойствам родителя. Переопределение свойств."
+# Цели: Применить сокрытие атрибутов и повторить наследование. Рассмотреть на примере объекта из реального мира.
+
+# Необходимо создать 2 класса: Vehicle и Sedan, где Vehicle - это любой транспорт,
+# а Sedan(седан) - наследник класса Vehicle.
+#
+# I. Каждый объект Vehicle должен содержать следующие атрибуты объекта:
+# Атрибут owner(str) - владелец транспорта. (владелец может меняться)
+# Атрибут __model(str) - модель (марка) транспорта. (мы не можем менять название модели)
+# Атрибут __engine_power(int) - мощность двигателя. (мы не можем менять мощность двигателя самостоятельно)
+# Атрибут __color(str) - название цвета. (мы не можем менять цвет автомобиля своими руками)
+# А так же атрибут класса:
+# Атрибут класса __COLOR_VARIANTS, в который записан список допустимых цветов для окрашивания. (Цвета написать свои)
+# Каждый объект Vehicle должен содержать следующий методы:
+# Метод get_model - возвращает строку: "Модель: <название модели транспорта>"
+# Метод get_horsepower - возвращает строку: "Мощность двигателя: <мощность>"
+# Метод get_color - возвращает строку: "Цвет: <цвет транспорта>"
+# Метод print_info - распечатывает результаты методов (в том же порядке): get_model, get_horsepower, get_color;
+#   а так же владельца в конце в формате "Владелец: <имя>"
+# Метод set_color - принимает аргумент new_color(str), меняет цвет __color на new_color,
+#   если он есть в списке __COLOR_VARIANTS, в противном случае выводит на экран надпись:
+#   "Нельзя сменить цвет на <новый цвет>".
+# Взаимосвязь методов и скрытых атрибутов:
+# Методы get_model, get_horsepower, get_color находятся в одном классе с соответствующими им
+#   атрибутами: __model, __engine_power, __color. Поэтому атрибуты будут доступны для методов.
+# Атрибут класса __COLOR_VARIANTS можно получить обращаясь к нему через объект(self).
+# Проверка в __COLOR_VARIANTS происходит не учитывая регистр ('BLACK' ~ 'black').
+# II. Класс Sedan наследуется от класса Vehicle, а так же содержит следующие атрибуты:
+# Атрибут __PASSENGERS_LIMIT = 5 (в седан может поместиться только 5 пассажиров)
+
+from colorama import Fore, Style
+
+
+class Vehicle:
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+
+    def __init__(self, owner, model, color, engine_power):
+        self.owner = str(owner)  # владелец транспорта.(владелец может меняться)
+        self.__model = str(model)  # модель (марка) транспорта. (мы не можем менять название модели)
+        self.__engine_power = int(
+            engine_power)  # мощность двигателя. (мы не можем менять мощность двигателя самостоятельно)
+        self.__color = str(color)  # название цвета. (мы не можем менять цвет автомобиля своими руками)
+
+    def get_model(self):
+        return f"Модель: {self.__model}"
+
+    def get_horsepower(self):
+        return f"Мощность двигателя: {self.__engine_power}"
+
+    def get_color(self):
+        return f"Цвет: {self.__color}"
+
+    def print_info(self):
+        print(f'{Vehicle.get_model(self)} \n{Vehicle.get_horsepower(self)} \n{Vehicle.get_color(self)} '
+              f'\nВладелец: {self.owner}')
+
+    def set_color(self, new_color):
+        self.new_color = str(new_color)
+        if self.new_color.lower() in self.__COLOR_VARIANTS:
+            self.__color = self.new_color
+        else:
+            print(Fore.RED + f'Нельзя сменить цвет на {self.new_color}', Style.RESET_ALL)
+
+
+class Sedan(Vehicle):
+    __PASSENGERS_LIMIT = 5
+
+    def __init__(self, owner, model, color, engine_power):
+        super().__init__(owner, model, color, engine_power)
+
+
+# Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
+
+# Изначальные свойства
+vehicle1.print_info()
+
+# Меняем свойства (в т.ч. вызывая методы)
+vehicle1.set_color('Pink')
+vehicle1.set_color('BLACK')
+vehicle1.owner = 'Vasyok'
+
+# Проверяем что поменялось
+vehicle1.print_info()
