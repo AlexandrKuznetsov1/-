05cefd1bdcef8ef1451819a864932a59fdd42926
AlexandrKuznetsov1 <166503929+AlexandrKuznetsov1@users.noreply.github.com>
2024-06-24 15:16:43 +0700

    Add files via upload

---
+++ Modul4/Module_4_1/true_math.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from math import inf
+
+# Домашняя работа по уроку "Модули и пакеты"
+# Цель: закрепить навык создания и импортирования модулей, а так же функций и переменных находящихся в них.
+
+def divide(first, second):
+    if second != 0:
+        return first / second
+    else:
+        return inf
